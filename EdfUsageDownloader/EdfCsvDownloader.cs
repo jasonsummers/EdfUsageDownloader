f39ef77233ed3ad5d6e2001b71b961cb9e334b02
Jason Summers <3616919+JasonS-Dev@users.noreply.github.com>
2023-07-01 10:34:18 +0100

    Move the cookie consent dialog check to account for revised website design.

--- EdfUsageDownloader/EdfCsvDownloader.cs
+++ EdfUsageDownloader/EdfCsvDownloader.cs
@@ -130,15 +130,15 @@ public class EdfCsvDownloader : IEdfDataProducer, IDisposable
 
         // Fill "input[name=\"email\"]"
         await _page.FillAsync("input[name=\"email\"]", _email);
+        
+        var cookiesButton = _page.Locator("[aria-label=\"Let us know if we can use cookies\"] [aria-label=\"Close\"]");
+        if (cookiesButton is not null) await cookiesButton.ClickAsync();
 
         // Click text=Next
         await _page.ClickAsync("button[aria-label=\"Log in\"]");
 
         // Fill [placeholder="Password"]
         await _page.FillAsync("[placeholder=\"Password\"]", _password);
-
-        var cookiesButton = _page.Locator("[aria-label=\"Let us know if we can use cookies\"] [aria-label=\"Close\"]");
-        if (cookiesButton is not null) await cookiesButton.ClickAsync();
 
         // Click text=Log in with password
         await _page.ClickAsync("button#customer_login");
